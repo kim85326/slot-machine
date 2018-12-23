e61319e528d4acc08c241c132ab28d8c6e33e419
elaine <kim85326@gmail.com>
2018-12-23 21:23:22 +0800

    Updates

---
+++ precache-manifest.47f79d1017d9b56867fa9dfe310e1d0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d931611f4844b33da7e",
+    "url": "/slot-machine/static/js/runtime~main.2d931611.js"
+  },
+  {
+    "revision": "bfdebebaf7c7977ba387",
+    "url": "/slot-machine/static/js/main.bfdebeba.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/slot-machine/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "bfdebebaf7c7977ba387",
+    "url": "/slot-machine/static/css/main.53a2afd9.chunk.css"
+  },
+  {
+    "revision": "a47c21c0850adbc8d31c77cb2c43f98b",
+    "url": "/slot-machine/index.html"
+  }
+];
